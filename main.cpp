eb5154b875fb3b38871417a05a67a267eb2a0bd2
Nikita Krenov <nikitabaram@gmail.com>
2024-06-16 15:07:07 +0300

    Two arrays are given: A[M] and B[N] (M and N are entered from the keyboard). It is necessary to create a third array of the smallest possible size, in which the elements of the arrays A and B that are not common to them must be collected without repetition.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,72 @@
 #include <string>;
 using namespace std;
 
+bool my_fun(int* arr, int size, int element) {
+	for (int i = 0; i < size; ++i) {
+		if (*(arr + i) == element) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool in_arr(int* arr, int size, int element) {
+	for (int i = 0; i < size; ++i) {
+		if (*(arr + i) == element) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
+	int M, N;
+	int* A = new int[M];
+	cout << "Enter the M: ";
+	cin >> M;
+	cout << "Enter the elements of the first array: ";
+	
+	for (int i = 0; i < M; ++i) {
+		cout << "Enter the " << i + 1 << " element: ";
+		cin >> A[i];
+	}
+
+	cout << "Enter the N: ";
+	cin >> N;
+	int* B = new int[N];
+	cout << "Enter the elements of the second array: ";
+	for (int i = 0; i < N; ++i) {
+		cout << "Enter the " << i + 1 << " element: ";
+		cin >> B[i];
+	}
+
+	int max_width = M + N;
+	int* C = new int[max_width];
+	int k = 0;
+
+	for (int i = 0; i < M; ++i) {
+		if (!in_arr(C, k, A[i])) {
+			C[k] = A[i];
+			++k;
+		}
+	}
+
+	for (int i = 0; i < N; ++i) {
+		if (!in_arr(C, k, B[i])) {
+			C[k] = B[i];
+			++k;
+		}
+	}
+
+	cout << "Unique elements in A and B that are not common: ";
+	for (int i = 0; i < k; ++i) {
+		cout << C[i] << " ";
+	}
+	cout << endl;
+
+	delete[] A;
+	delete[] B;
+	delete[] C;
 
 	system("pause");
 	return 0;
